9ad2831e59ef27a7e7476472bdb31eed983bba84
Sofía Medina <s.nicolmedina@gmail.com>
2023-08-09 11:43:53 -0300

    Initial commit

---
+++ equipos.js
@@ -0,0 +1,14 @@
+function rotateImage(element) {
+    element.classList.toggle('rotated');
+}
+
+function rotateImage(element) {
+    element.classList.toggle('rotated');
+}
+
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('nav-menu-visible');
+});
